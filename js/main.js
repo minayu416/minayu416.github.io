c84bb384b8d99b6e3eeec8e689b6fcad418157fe
Ming-Jung YU <minayu416@gmail.com>
2022-10-02 18:37:47 +0800

    publish test website

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// var x = document.getElementById("mobile-menu");
+// $("body").click(function(){
+//     if (x.classList.item(x.classList.length - 1) == "hidden"){
+//         x.classList.remove("hidden");
+//       }
+//   });
