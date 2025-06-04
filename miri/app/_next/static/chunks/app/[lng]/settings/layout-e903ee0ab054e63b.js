0c5dbb4a9fadf6a5a1339f42ac197bbf86932515
Ming-Jung YU <minayu416@gmail.com>
2025-06-04 21:20:28 +0800

    remove miri app

--- miri/app/_next/static/chunks/app/[lng]/settings/layout-e903ee0ab054e63b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[907,419,737,641,421,303,867,583,264,478],{3521:function(){}},function(n){n.O(0,[971,69,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
