3ba5ceb48fc4798b9f28a326302dd4805145e1fc
Ming-Jung YU <minayu416@gmail.com>
2025-07-11 16:40:00 +0800

    updated resume format

---
+++ _next/static/chunks/app/en/layout-da5ad95ed24c57e7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[201],{5979:function(e,n,_){Promise.resolve().then(_.t.bind(_,8549,23))},8549:function(e){e.exports={style:{fontFamily:"'__Inter_e8ce0c', '__Inter_Fallback_e8ce0c'",fontStyle:"normal"},className:"__className_e8ce0c"}}},function(e){e.O(0,[188,971,23,744],function(){return e(e.s=5979)}),_N_E=e.O()}]);
