8e34cd6d2c40c75db17b6a871016d33ad222d5cc
Ming-Jung YU <minayu416@gmail.com>
2026-01-29 16:13:18 +0800

    developed dark mode theme. and re-design some light theme.

---
+++ _next/static/chunks/app/en/layout-b10857ee4d69ce85.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[201],{5979:function(n,e,_){Promise.resolve().then(_.t.bind(_,8549,23))},8549:function(n){n.exports={style:{fontFamily:"'__Inter_f367f3', '__Inter_Fallback_f367f3'",fontStyle:"normal"},className:"__className_f367f3"}}},function(n){n.O(0,[188,971,23,744],function(){return n(n.s=5979)}),_N_E=n.O()}]);
--- _next/static/chunks/app/en/page-dd2ab9a32a705c4d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[479,931],{4882:function(e,n,t){Promise.resolve().then(t.bind(t,5758)),Promise.resolve().then(t.bind(t,619))},6463:function(e,n,t){"use strict";var u=t(1169);t.o(u,"usePathname")&&t.d(n,{usePathname:function(){return u.usePathname}})}},function(e){e.O(0,[340,971,23,744],function(){return e(e.s=4882)}),_N_E=e.O()}]);
